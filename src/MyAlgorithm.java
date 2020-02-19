8c75648f47280b036545eb391d3d60f68d27c8ff
Vita <wiebetc@gmail.com>
2020-02-19 13:24:56 -0800

    - Bout to do more craziness: - Need to reassign spots within itself or SelectionSort won't work: selectionSort works on the premise that the first part of the list is sorted;

--- src/MyAlgorithm.java
+++ src/MyAlgorithm.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 /**
  * @author Vita.Wiebe@seattlecolleges.edu
  *
@@ -7,87 +5,85 @@ import java.util.Arrays;
 public class MyAlgorithm {
 
     /**
+     * Pre: A filled 2D array to be sorted;
+     * Post: Print the input array, but sorted in ascending order according to the first
+     * element of the inner (column) element;
      * @param inputArray a 2D array of ints to be sorted according to first index elem of
      *                   each internal array;
-     * @return outputArray, the algorithm-sorted version of our inputArray;
      */
     public static int[][] impAlgo(int[][] inputArray) {
 
-        // 'Size' allows the method to be dynamic and easily refactorable;
-//        int size = inputArray.length;
-
         // Create new array for output;
         int[][] outputArray = new int[5][5];
-
-        /*
-         Initialize min to the value of the first element of the first array in
-         our inputArray;
-         Use 'min' to compare the values of the first elements of each internal array;
-        */
-        int min;
 
         /*
          Only iterate through rows, since we are unconcerned with any columns
          save for the first one in each row (each internal array of 'inputArray');
         */
         for (int row = 0; row < 5; row++) {
-            min = inputArray[row][0];
             int remainder;
-            // tempArray holds whichever array holds the current min as its first element;
-            int[] tempArray;
+            /*
+             Initialize min to the value of the first element of the first array in
+             our inputArray;
+             Use 'min' to compare the values of the first elements of each internal array;
+            */
+            int min = inputArray[row][0];
+            // 'windex' will be used to store the row index of the next array to
+            // place in outArray;
+            int windex = 0;
+            // Selection sort: search rest of array for next minimum element at head;
             for (remainder = row + 1; remainder < 5; remainder++) {
                 // Test next first value of internal array against min;
                 if (inputArray[remainder][0] < min) {
                     // Reassign value of 'min' if that element is < min;
                     min = inputArray[remainder][0];
-                    // Reassign tempArray so as to keep a reference to this internal array;
-                    tempArray = inputArray[remainder];
+                    // 'windex' tracks the winning row index of inputArray;
+                    windex = remainder;
                 }
-
-//                outputArray[remainder] = Arrays.copyOf(tempArray);
-//                }
-//                }
-//            }
-//        }
-//        return outputArray;
             }
 
-            public static void main (String[]args){
-
-                // Our array to test algorithm;
-                int[][] testArray = {
-                        {1, 2, 3, 4, 5},
-                        {3, 4, 5, 1, 2},
-                        {5, 2, 3, 4, 1},
-                        {2, 3, 1, 4, 5},
-                        {4, 2, 3, 1, 5}
-                };
-                int[][] test = new int[5][5];
-                int[] test2 = {1, 2, 3, 4, 5};
-                for (int[] elem : test) {
-                    elem = test2;
-                }
-                System.out.println(test[0][2]);
-
-
-                //int[][] newArray = impAlgo(testArray);
-//        for (int[] elem : newArray) {
-//            System.out.print("" + elem + "");
-//        }
-//        for (int row = 0; row < 5; row++) {
-//            for (int col = 0; col < 5; col++) {
-//                System.out.print(newArray[row][col] + ", ");
-//            }
-//            System.out.println();
-//        }
-
-                // System.out.println(testArray[row(array)][col(element)]);
-                // Done to understand how data being passed;
-//        System.out.println(testArray[0][0]);
-//        System.out.println(testArray[1][0]);
-//        System.out.println(testArray[2][0]);
+            // Copy elements in winning row
+            // of internal array to next internal array in outputArray;
+            for (int col = 0; col < 5; col++) {
+                outputArray[row][col] = inputArray[windex][col];
             }
+
+            // Print stuff:
+            // Fencepost: print first item in list with opening curly brace;
+//            System.out.print("{" + outputArray[row][0] + ",");
+//            // Print middle of next inner array, comma-separated;
+//            for (int col = 1; col < 4; col++) {
+//                System.out.print(outputArray[row][col] + ",");
+//            }
+//            // Fencepost end: print last value in inner array, close with curly brace;
+//            System.out.println(outputArray[row][4] + "}");
         }
+        return outputArray;
+    }
 
+    /**
+     * Our application method;
+     * Use dummy 2D array, 'testArray', to test the impAlgo() method above;
+     * @param args fuck
+     */
+    public static void main (String[] args) {
+
+        // Our array to test algorithm;
+        int[][] testArray = {
+                {1, 2, 3, 4, 5},
+                {3, 4, 5, 1, 2},
+                {5, 2, 3, 4, 1},
+                {2, 3, 1, 4, 5},
+                {4, 2, 3, 1, 5}
+        };
+        int[][]nuArray = impAlgo(testArray);
+        for(int row = 0; row < 5; row++) {
+            for (int col = 0; col < 5; col++) {
+                System.out.print(nuArray[row][col]);
+            }
+            System.out.println();
+        }
     }
 }
+
+
